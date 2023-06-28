0264c69abf0b11df8624ac0c9249252ad3639598
Raymond Veldhuizen <raymond.veldhuizen@id-t.com>
2023-06-28 11:30:23 +0200

    initial commit with first working plugin

---
+++ README.md
@@ -0,0 +1,19 @@
+# Akamai EdgeGrid Authorization for Insomnia
+
+This is a 'updated' plugin for [Insomnia Client](https://insomnia.rest/). This replaces the version of [insomnia-plugin-edgegrid](https://github.com/schwabix/insomnia-plugin-edgegrid)
+
+Akamai EdgeGrid Authorization: https://developer.akamai.com/legacy/introduction/Client_Auth.html
+
+## Install
+
+Go to Preferences->Plugins, install `insomnia-plugin-akami-edgegrid`
+
+## How to use
+
+1. Setup your environment to include your .edgerc configuration
+
+![Screenshot](https://raw.githubusercontent.com/rveldhuizen/insomnia-plugin-akami-edgegrid/main/readme-ss-environment.png)
+
+2. Add a "dummy" header EdgeAuthorization - this triggers the generation of the Akamai Authorization header
+
+![Screenshot](https://raw.githubusercontent.com/rveldhuizen/insomnia-plugin-akami-edgegrid/main/readme-ss-edgegrid-header.png)
---
+++ main.js
@@ -0,0 +1,29 @@
+const EdgeGrid = require('akamai-edgegrid');
+
+module.exports.requestHooks = [
+    context => {
+        // Use dummy header to trigger generation of Akamai API Authorization header
+        if (context.request.hasHeader('EdgeAuthorization')) {
+            context.request.removeHeader('EdgeAuthorization');
+        }
+
+        // Get edgerc settings from environment
+        const clientToken = context.request.getEnvironmentVariable("client_token");
+        const clientSecret = context.request.getEnvironmentVariable("client_secret");
+        const accessToken = context.request.getEnvironmentVariable("access_token");
+        const baseUri = "https://" + context.request.getEnvironmentVariable("host") + "/";
+
+        const headers = context.request.getHeaders();
+        // Generate headers with akamai-edgegrid
+        const eg = new EdgeGrid(clientToken, clientSecret, accessToken, baseUri);
+        eg.auth({
+            path: context.request.getUrl().replace(baseUri, ""),
+            method: context.request.getMethod(),
+            headers: headers,
+            body: context.request.getBodyText()
+        });
+
+        // Set header on current request
+        context.request.setHeader('Authorization', eg.request.headers.Authorization);
+    }
+]
