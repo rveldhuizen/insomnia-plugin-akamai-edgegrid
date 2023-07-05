e56ddf1892a0a9da6e041db9cc18f358996797ad
Raymond Veldhuizen <rveldhuizen@gmail.com>
2023-07-05 07:55:58 +0000

    fix: wrap event headers within statement

--- main.js
+++ main.js
@@ -5,25 +5,25 @@ module.exports.requestHooks = [
         // Use dummy header to trigger generation of Akamai API Authorization header
         if (context.request.hasHeader('EdgeAuthorization')) {
             context.request.removeHeader('EdgeAuthorization');
+
+            // Get edgerc settings from environment
+            const clientToken = context.request.getEnvironmentVariable("client_token");
+            const clientSecret = context.request.getEnvironmentVariable("client_secret");
+            const accessToken = context.request.getEnvironmentVariable("access_token");
+            const baseUri = "https://" + context.request.getEnvironmentVariable("host") + "/";
+
+            const headers = context.request.getHeaders();
+            // Generate headers with akamai-edgegrid
+            const eg = new EdgeGrid(clientToken, clientSecret, accessToken, baseUri);
+            eg.auth({
+                path: context.request.getUrl().replace(baseUri, ""),
+                method: context.request.getMethod(),
+                headers: headers,
+                body: context.request.getBodyText()
+            });
+
+            // Set header on current request
+            context.request.setHeader('Authorization', eg.request.headers.Authorization);
         }
-
-        // Get edgerc settings from environment
-        const clientToken = context.request.getEnvironmentVariable("client_token");
-        const clientSecret = context.request.getEnvironmentVariable("client_secret");
-        const accessToken = context.request.getEnvironmentVariable("access_token");
-        const baseUri = "https://" + context.request.getEnvironmentVariable("host") + "/";
-
-        const headers = context.request.getHeaders();
-        // Generate headers with akamai-edgegrid
-        const eg = new EdgeGrid(clientToken, clientSecret, accessToken, baseUri);
-        eg.auth({
-            path: context.request.getUrl().replace(baseUri, ""),
-            method: context.request.getMethod(),
-            headers: headers,
-            body: context.request.getBodyText()
-        });
-
-        // Set header on current request
-        context.request.setHeader('Authorization', eg.request.headers.Authorization);
     }
 ]
